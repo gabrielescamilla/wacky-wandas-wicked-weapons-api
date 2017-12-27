ffeef2a58839fdbd65f695dfbe4f88998c33cd9b
Greg Malcolm <greg@gregmalcolm.com>
2017-12-26 21:22:00 -0500

    Tweaked slowdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   before_action if: "Rails.env.development?" do
     # Artificial lag to make up for demoing without internet
-    sleep 0.1 + (Random.rand * 0.5)
+    sleep 0.6 + (Random.rand * 1)
   end
 end
